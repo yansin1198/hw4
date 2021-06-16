1db23fc7a751c444bc3b698566f1d2d11d87c0c0
yansin1198 <59250738+yansin1198@users.noreply.github.com>
2021-06-16 15:39:40 +0800

    Update main.py

--- Following_BBCar/main.py
+++ Following_BBCar/main.py
@@ -15,6 +15,7 @@ sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
 sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
 clock = time.clock()
 
+data = 0
 detectLine = 0
 stop = 0
 
@@ -24,20 +25,40 @@ while(True):
     if enable_lens_corr: img.lens_corr(1.8) # for 2.8mm lens...
 
     for l in img.find_line_segments(merge_distance = 0, max_theta_diff = 5):
-        #if l.magnitude() > 3:
-        if l.y2() == 0 & stop == 0:
-            print(l)
+        if l.y2() == 0 : #& l.x1() >= 25 & l.x2() >= 5 & l.y1() >= 40 & l.x1() <= 129 & l.x2() <= 140:
             img.draw_line(l.line(), color = (255, 0, 0))
             detectLine = 1
-            uart.write(("%d" % detectLine).encode())
             print("detectLine = %d" % detectLine)
-            if l.length() <= 11 :
-                #print(l)
-                detectLine = 0
-                stop = 1
-                img.draw_line(l.line(), color = (0, 255, 0))
-                uart.write(("%d" % detectLine).encode())
+            #uart.write(("%d" % detectLine).encode())
+
+            data = (l.x1(), l.x2(), l.y1(), l.y2())
+            uart.write(("1 %d %d %d %d\n" % data).encode())
+
+            if l.x1() - l.x2() <= 2 & l.x1() - l.x2() >= 0: #right
+                detectLine = 2
                 print("detectLine = %d" % detectLine)
+                #uart.write(("%d" % detectLine).encode())
+
+                data = (l.x1(), l.x2(), l.y1(), l.y2())
+                uart.write(("2 %d %d %d %d\n" % data).encode())
+                print(data)
+
+                img.draw_line(l.line(), color = (0, 255, 0))
+
+            elif l.x1() - l.x2() >= -2 & l.x1() - l.x2() <= 0:
+                detectLine = 3
+                print("detectLine = %d" % detectLine)
+                #uart.write(("%d" % detectLine).encode())
+
+                data = (l.x1(), l.x2(), l.y1(), l.y2())
+                uart.write(("3 %d %d %d %d\n" % data).encode())
+                print(data)
+
+                img.draw_line(l.line(), color = (0, 255, 0))
+
+            else:
+                detectLine = 0
+                uart.write(("0 %d %d %d %d\n" % data).encode())
 
 
 
