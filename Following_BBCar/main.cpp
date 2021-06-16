52ae7833093f370d46d7a34b694293a8201bd180
yansin1198 <59250738+yansin1198@users.noreply.github.com>
2021-06-16 15:38:50 +0800

    Update main.cpp

--- Following_BBCar/main.cpp
+++ Following_BBCar/main.cpp
@@ -6,38 +6,79 @@ Ticker servo_ticker;
 
 PwmOut pin5(D5), pin6(D6);
 
-BufferedSerial pc(USBTX, USBRX);
-//BufferedSerial xbee(USBTX, USBRX);
+BufferedSerial pc(D1, D0);
+BufferedSerial xbee(USBTX, USBRX);
 BufferedSerial uart(D10, D9); //tx,rx
 BBCar car(pin5, pin6, servo_ticker);
 
-/*void parking(Arguments *in, Reply *out);
-RPCFunction rpcparking(&parking, "parking");
-double d1, d2;
-char dir;
-int waitTime;*/
+void linefollow(Arguments *in, Reply *out);
+RPCFunction rpclinefollow(&linefollow, "linefollow");
+double state = 0;
+double x, y, m;
 
+int point[5];
 
-int main(){
-   uart.set_baud(9600);
-   while(1){
-      if(uart.readable()){
-            char recv[1];
-            uart.read(recv, sizeof(recv));
-            pc.write(recv, sizeof(recv));
-            printf("\r\n");
+void compare(int x1, int x2) {
+   
+   if((x1 - x2 >= -2 && x1 - x2 <= 2)) { //|| (x2 - x1 >= 0 && x2 - x1 <= 2)
+      car.goStraight(50);
+      printf("goStraight\r\n");
+      ThisThread::sleep_for(1500ms);
+   }
+   else if(x1 > x2) { //&& (x1 - x2 <= 5) && (x1 - x2 > 2)) {
+      car.turn(-35, -0.2); 
+      printf("turn left\r\n");
+      ThisThread::sleep_for(500ms);
+   }
+   else if(x1 < x2) {// && (x2 - x1 <= 5) && (x2 - x1 > 2)) { 
+      car.turn(-40, 0.1);
+      printf("turn right\r\n");
+      ThisThread::sleep_for(500ms);
+   }
+    car.stop();
+    ThisThread::sleep_for(500ms);
+}
 
-        if (recv[0] == '1') {
-                printf("go straight\r\n");
-                car.goStraight(50);
-                ThisThread::sleep_for(500ms);
-                car.stop();
+void linefollow(Arguments *in, Reply *out) {
 
-        } else { 
-            printf("stop\r\n");
-            car.stop();
+    uart.set_baud(9600);
+    state = in->getArg<double>();
+    printf("get\r\n");
+
+    while (state) {
+        if(uart.readable()) {
+           for(int i = 0;;i++) {
+                char recv[1];
+                uart.read(recv, sizeof(recv));
+                pc.write(recv, sizeof(recv));
+                point[i] = (int)recv[0];
+                if(recv[0] =='\n') {
+                    break;
+                }
+            }
+
+            printf("x1, x2, y1, y2: %d, %d, %d, %d \r\n", point[0], point[1], point[2], point[3]);
+
+            compare(point[0], point[1]);
         } 
-        ThisThread::sleep_for(500ms);
+    }
+}
+
+
+int main() {
+   char buf[256], outbuf[256];
+   FILE *devin = fdopen(&xbee, "r");
+   FILE *devout = fdopen(&xbee, "w");
+   while (1) {
+      memset(buf, 0, 256);
+      for( int i = 0; ; i++ ) {
+         char recv = fgetc(devin);
+         if(recv == '\n') {
+            printf("\r\n");
+            break;
+         }
+         buf[i] = fputc(recv, devout);
       }
+   RPC::call(buf, outbuf);
    }
 }
